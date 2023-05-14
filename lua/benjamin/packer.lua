cb1d0177ef13b880c73aee8299362cd4ad144a4f
Fritz-Lakritz <benjamin.koerfer@t-online.de>
2023-05-14 14:50:22 +0200

    made changes

--- lua/benjamin/packer.lua
+++ lua/benjamin/packer.lua
@@ -5,8 +5,8 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   -- Packer can manage itself
-
   use 'wbthomason/packer.nvim'
+  use 'ThePrimeagen/vim-be-good'
 
   use {'neoclide/coc.nvim', branch = 'release'}
 
