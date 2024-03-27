f703937bc23ab4f15507644481c6279589a76939
Fritz-Lakritz <benjamin.koerfer@t-online.de>
2024-03-27 19:09:49 +0100

    lazy

---
+++ lua/core/set.lua
@@ -0,0 +1,53 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- test
+-- test
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+vim.opt.smartindent = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.mouse = 'a'
+
+vim.opt.spelllang = { 'de', 'en' }
+
+vim.opt.spell = true
+
+vim.o.autoindent = true -- Enable auto-indentation
+vim.o.expandtab = true  -- Use spaces instead of tabs
+vim.o.shiftwidth = 4    -- Set the number of spaces inserted for each indentation level
+
+vim.opt.clipboard = 'unnamedplus'
+
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.signcolumn = 'yes'
+
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
+
+vim.opt.hlsearch = true
+
+vim.opt.laststatus = 3
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,18 +1,27 @@
-return{
-	'nvim-telescope/telescope.nvim',
-	dependencies = {
-		'nvim-lua/plenary.nvim',
-		{
-			'telescope-fzf-native.nvim',
-			build = 'make',
-			cond = function()
-				return vim.fn.executable 'make' == 1
-			end
-		},
-	},
-	config = function()
-		require('telescope').setup{}
-		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
-	end
+return {
+    'nvim-telescope/telescope.nvim',
+    event = 'VimEnter',
+    branch = '0.1.x',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+    },
+    config = function()
+        require('telescope').setup {}
+
+        pcall(require('telescope').load_extension, 'fzf')
+        -- keymaps
+
+        local builtin = require 'telescope.builtin'
+        vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+        vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+        vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+        vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+        vim.keymap.set('n', '<leader>sg', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+        vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+        vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+        vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+    end,
 }
