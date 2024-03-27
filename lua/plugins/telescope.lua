41c37e6cb52ff0162aa9893ba7420d6b3487c184
Fritz-Lakritz <benjamin.koerfer@t-online.de>
2024-03-27 17:37:51 +0100

    lazy

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,18 @@
 return{
-'nvim-telescope/telescope.nvim', tag = '0.1.6',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+	'nvim-telescope/telescope.nvim',
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		{
+			'telescope-fzf-native.nvim',
+			build = 'make',
+			cond = function()
+				return vim.fn.executable 'make' == 1
+			end
+		},
+	},
+	config = function()
+		require('telescope').setup{}
+		local builtin = require('telescope.builtin')
+		vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
+	end
 }
